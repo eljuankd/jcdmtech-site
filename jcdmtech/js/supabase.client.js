4cae411bf5c5bf88fc35ac5666f6ef6f0c05d37f
Juank <42949107+eljuankd@users.noreply.github.com>
2025-09-23 16:08:15 -0400

    supa_1.1 correction

--- jcdmtech/js/supabase.client.js
+++ jcdmtech/js/supabase.client.js
@@ -1,5 +1,4 @@
-<script type="module">
   import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
   const cfg = window.__SUPABASE__;
   window.supabase = createClient(cfg.url, cfg.anon);
-</script>
+
--- jcdmtech/js/supabase.config.js
+++ jcdmtech/js/supabase.config.js
@@ -1,6 +1,5 @@
-<script>
   window.__SUPABASE__ = {
     url: "https://fsqfialyjhpalxbdjktd.supabase.co",
     anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzcWZpYWx5amhwYWx4YmRqa3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTI1NDEsImV4cCI6MjA3NDIyODU0MX0.AQUH4cbTjGQFW_S_TxterRLBcRbSEzPp9svaWLCt2XE"
   };
-</script>
+
